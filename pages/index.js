fd694cd1ba9967c9f31e09b22818b7ee9b531d0e
Mohammad Abuosbie <mohammad@Mohammads-MacBook-Pro.local>
2023-02-02 17:08:10 -0600

    Rename title

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Simple Blockchain AI</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
